f36f5e0908caef69b061e9520413112252355a75
Péter HANDBAUER <peter.handbauer@nng.com>
2017-08-11 14:26:53 +0200

    modpow function added

--- 20170804/second.cpp
+++ 20170804/second.cpp
@@ -4,9 +4,25 @@
 
 using namespace std;
 
+template <typename T>
+T modpow(T base, T exp, T modulus)
+{
+    base %= modulus;
+    T result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = (result * base) % modulus;
+        base = (base * base) % modulus;
+        exp >>= 1;
+    }
+    return result;
+}
+
 unsigned long long numberOfAntiPalidromicStrings(int N, int M)
 {
-    // TODO
+    // TODO fix it for small numbers!
+    return M * (M-1) * modpow(M-2, N-2, 1000000007);
 }
 
 int main()
